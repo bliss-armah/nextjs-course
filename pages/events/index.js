56be4bb16b79e82103eb49d67d354df5d70be42b
Bliss  Armah-Nwanwah <blissarmah8@gmail.com>
2023-06-09 10:38:08 +0000

    search component structured

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,19 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+import { useRouter } from "next/router";
+
 const AllEventPage = () => {
   const events = getAllEvents();
+  const router = useRouter()
+
+  const findEventHandler = (year,month) =>{
+    const fullpath = `/events/${year}/${month}`
+router.push(fullpath)
+  }
   return (
     <div>
+        <EventsSearch onSearch={findEventHandler} />
       <EventList items={events} />
     </div>
   );
