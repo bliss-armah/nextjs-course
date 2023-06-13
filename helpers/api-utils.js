830d85d377cba1fe1cb185e0a144c958aa2ef0d6
Bliss  Armah-Nwanwah <blissarmah8@gmail.com>
2023-06-13 13:25:09 +0000

    adding ssg to the home page

---
+++ helpers/api-utils.js
@@ -0,0 +1,21 @@
+export async function getAllEvents(){
+ const response =  await fetch('https://nextjs-course-52920-default-rtdb.firebaseio.com/events.json');
+ const data = await response.json();
+ const events = []
+ 
+ for (const key in data) {
+     events.push({
+         id:key,
+         ...data[key],
+        });
+ }
+        // console.log(data);
+
+ return events;
+}
+
+export async function getFeaturedEvents() {
+    const allEvent = await getAllEvents();
+    return allEvent.filter((event) => event.isFeatured);
+  }
+  
