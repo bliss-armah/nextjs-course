7c164535475fe0e359a4bd7fd980306251f55537
Bliss  Armah-Nwanwah <blissarmah8@gmail.com>
2023-06-13 13:43:15 +0000

    adding ssg to the home page done

--- helpers/api-utils.js
+++ helpers/api-utils.js
@@ -1,21 +1,27 @@
-export async function getAllEvents(){
- const response =  await fetch('https://nextjs-course-52920-default-rtdb.firebaseio.com/events.json');
- const data = await response.json();
- const events = []
- 
- for (const key in data) {
-     events.push({
-         id:key,
-         ...data[key],
-        });
- }
-        // console.log(data);
+export async function getAllEvents() {
+  const response = await fetch(
+    "https://nextjs-course-52920-default-rtdb.firebaseio.com/events.json"
+  );
+  const data = await response.json();
+  const events = [];
 
- return events;
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+  // console.log(data);
+
+  return events;
 }
 
 export async function getFeaturedEvents() {
-    const allEvent = await getAllEvents();
-    return allEvent.filter((event) => event.isFeatured);
-  }
-  
+  const allEvent = await getAllEvents();
+  return allEvent.filter((event) => event.isFeatured);
+}
+
+export async function getEventById(id) {
+  const allEvent = await getAllEvents();
+  return allEvent.find((event) => event.id === id);
+}
